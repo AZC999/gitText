aa2c5da10e9cb28762321d222b58ba049a38b9dc
AZC999 <3193908527@qq.com>
2019-11-13 20:29:18 +0800

    修改

--- js/a_shouye.js
+++ js/a_shouye.js
@@ -1,47 +1,4 @@
-//  <script>
-//   var oLis = document.querySelectorAll('li');
-//   var oImgs = document.querySelectorAll('.a_swiperBox img');
 
-//   var index = 0;
-//   var timer;
-
-//   start();
-
-//   for (var i = 0; i < oLis.length; i++) {
-//     oLis[i].index = i;
-//     oLis[i].onmouseenter = function () {
-//       clearInterval(timer);
-//       // this.index
-//       index = this.index;
-//       activeOne();
-//     }
-
-//     oLis[i].onmouseleave = function () {
-//       start();
-//     }
-//   }
-
-//   function activeOne() {
-//     // 清除所有的激活状态
-//     for (var i = 0; i < oLis.length; i++) {
-//       oImgs[i].classList.remove('active');
-//       oLis[i].classList.remove('active');
-//     }
-
-//     oImgs[index].classList.add('active');
-//     oLis[index].classList.add('active');
-//   }
-
-//   function start() {
-//     timer = setInterval(function () {
-//       index++;
-//       if (index > oLis.length - 1) {
-//         index = 0;
-//       }
-//       activeOne();
-//     }, 2000);
-//   }
-// </script>
 <script>
  window.onscroll = function () {
 	var topContent=head.offsetHeight+logo.offsetHeight;
@@ -56,4 +13,15 @@
      a2_nav.style.display = 'none';
     }
   }
+ </script>
+ <script type="text/javascript">   
+     var video1=document.getElementById("videoPlay1");
+     video1.onclick=function(){
+         video1.controls=true;
+         if(video1.paused){
+             video1.play();
+         }else{
+             video1.pause();
+         }
+     }
  </script>
