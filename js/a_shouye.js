4df0f3985351a0f0ee464375e75e1bdd3e713de8
hongjieyou <1421144254@qq.com>
2019-11-14 17:38:20 +0800

    123

--- js/a_shouye.js
+++ js/a_shouye.js
@@ -1,27 +1,27 @@
-// window.onscroll = function (){
-// 	var topContent = head.offsetHeight + logo.offsetHeight;
-//     var scrollTop = document.documentElement.scrollTop || document.body.scrollTop; // 获取滚动高度
-//     console.log(scrollTop, topContent.offsetHeight);
-//     if(scrollTop >= topContent.offsetHeight) { // 滚动高度 > 头部内容高度
-//     	a2_nav.style.display = 'block';
-//      	a2_nav.style.top = 0;
-//      	a2_nav.style.right = 0;
-// 	 	a2_nav.style.left = 0;
-//     } else {
-//      	a2_nav.style.display = 'none';
-//     }
-// }
-// var yaoVideo = document.getElementById("videoA")
-// function play(){
-//  	if(yaoVideo.paused)
-//  		yaoVideo.play()
-//  		else
-//  		yaoVideo.pause()
-// }
-// var yaoVideo1 = document.getElementById("videoB")
-// function play(){
-//  	if (yaoVideo.paused)
-//  	yaoVideo.play()
-//  	else
-//  	yaoVideo.pause()
-// }
+window.onscroll = function (){
+	var topContent = head.offsetHeight + logo.offsetHeight;
+    var scrollTop = document.documentElement.scrollTop || document.body.scrollTop; // 获取滚动高度
+    console.log(scrollTop, topContent.offsetHeight);
+    if(scrollTop >= topContent.offsetHeight) { // 滚动高度 > 头部内容高度
+    	a2_nav.style.display = 'block';
+     	a2_nav.style.top = 0;
+     	a2_nav.style.right = 0;
+	 	a2_nav.style.left = 0;
+    } else {
+     	a2_nav.style.display = 'none';
+    }
+}
+var yaoVideo = document.getElementById("videoA")
+function play(){
+ 	if(yaoVideo.paused)
+ 		yaoVideo.play()
+ 		else
+ 		yaoVideo.pause()
+}
+var yaoVideo1 = document.getElementById("videoB")
+function play(){
+ 	if (yaoVideo.paused)
+ 	yaoVideo.play()
+ 	else
+ 	yaoVideo.pause()
+}
