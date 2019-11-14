4eba11dc96dbb82ffc083e440e6d7fb36fe3c049
hongjieyou <1421144254@qq.com>
2019-11-14 18:48:05 +0800

    ok

--- js/collect.js
+++ js/collect.js
@@ -3,41 +3,36 @@ $(function(){
     $('.foot').load('foot.html');
 
     //加载数据
-    //{"shopUrl":http://localhost/project/gitText/img/f-W-01.jpg,
-    //"shopTitle"Joy&Peace/真美诗2018冬季新款羊绒皮尖头粗跟短筒女皮靴YPK28DD8:,
-    //"shopPrice":678,"shopSize":1,"shopColor":"绿色"}
-    console.log(localStorage.getItem(`${localStorage.getItem('uphone')}collect`))
-    var arr = JSON.stringify([localStorage.getItem(`${localStorage.getItem('uphone')}collect`)]);
+    var arr = localStorage.getItem(`${localStorage.getItem('uphone')}collect`);
     arr = JSON.parse(arr);
-    console.log(arr);
 
     //判断是否有收藏数据
     if(localStorage.getItem('uphone')){
         var obj;
         for(var i = 0 ; i < arr.length ;i++){
             obj = arr[i];
-            $('.uc_fav_c').html(`<li class="clearfix">
+            $('.uc_fav_c').append(`<li class="clearfix">
                 <span class="s_h">
                     <a href="#" class="bgd">置顶</a>
                     <input type="checkbox">
                 </span>
                 <a href="#" class="pro_img">
-                    <img src="${obj[shopUrl]}">
+                    <img src="${obj['shopUrl']}">
                 </a>
                 <div class="fav_pro_c clearfix">
                     <div class="pro_info">
                         <p>
-                            <a href="#">${obj[shopTitle]}</a>
+                            <a href="#">${obj['shopTitle']}</a>
                         </p>
                         <p>
                             <span class="cgray">
-                                颜色：<em class="f_gray">${obj[shopColor]}</em>&nbsp;&nbsp;  
-                                尺码：<em class="f_gray">${obj[shopSize]}</em>
+                                颜色：<em class="f_gray">${obj['shopColor']}</em>&nbsp;&nbsp;  
+                                尺码：<em class="f_gray">${obj['shopSize']}</em>
                             </span>
                         </p>
                     </div>
                     <div class="price_cs">
-                        <p>${obj[shopPrice]}</p>
+                        <p>${obj['shopPrice']}</p>
                     </div>
                     <div class="cz">
                         <p class="cz1">
